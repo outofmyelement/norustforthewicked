be1c3d11db12cc515e212bc760f86df838a0a7ad
outofmyelement <mackenzie.cooper99@gmail.com>
2020-02-11 16:30:54 -0800

    added pattern_matching exercise; chapter 6

---
+++ pattern_matching/target/debug/deps/pattern_matching-c97bd5326fd2cd81.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/mcooper/Documents/norustforthewicked/pattern_matching/target/debug/deps/pattern_matching-c97bd5326fd2cd81.rmeta: src/main.rs
+
+/mnt/c/Users/mcooper/Documents/norustforthewicked/pattern_matching/target/debug/deps/pattern_matching-c97bd5326fd2cd81.d: src/main.rs
+
+src/main.rs:
